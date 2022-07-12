b62e6ad53d243f4d2c5041100a678860d919518f
Jason Harris <jharris@skillstorm.com>
2022-07-12 13:55:30 -0400

    adding drop down skeleton to landing

--- frontend/MyMobileService/src/app/app.module.ts
+++ frontend/MyMobileService/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    DropDownListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
