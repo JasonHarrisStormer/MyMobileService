d4b7dbeba93aa7697a237186d639529c28211588
Jason Harris <jharris@skillstorm.com>
2022-07-12 14:07:17 -0400

    drop down added still needs fucntionality

--- frontend/MyMobileService/src/app/app.component.ts
+++ frontend/MyMobileService/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyMobileService';
+
+  //define drop down menu json info
+  public sites: { [key: string]: Object; }[] = [
+ 
+    { Name: 'Home', address: 'landing' },
+
+    { Name: 'Login', address: 'login' },
+
+    { Name: 'Account Management', address: 'account' },
+
+    { Name: 'Phones', address: 'phones' },
+
+    { Name: 'Plans', address: 'plans' }
+
+];
+
+  //mapping local data to fields property
+  public localFields: Object = {text: 'Name', value: 'address'};
+
+  //set the placeholder
+  public localWaterMark: string = 'Goto: ';
 }
