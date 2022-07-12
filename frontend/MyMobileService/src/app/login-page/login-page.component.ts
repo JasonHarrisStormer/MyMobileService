3f887b853f0c91e5d4c1be13109d728ddb6e15ed
Jason Harris <jharris@skillstorm.com>
2022-07-12 16:26:13 -0400

    created basic onSubmit in Login

--- frontend/MyMobileService/src/app/login-page/login-page.component.ts
+++ frontend/MyMobileService/src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  public submitLogin(Username: string, Password: string){
+
+  }
   constructor() { }
 
   ngOnInit(): void {
