1354a1238744973699ba427381bee2d0e3d3ffd7
Jason Harris <jharris@skillstorm.com>
2022-07-13 16:46:11 -0400

    Added Submit Logic/failure handling for Login Page

--- frontend/MyMobileService/src/app/login-page/login-page.component.ts
+++ frontend/MyMobileService/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,11 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  passWord!: string;
+  userName!: string;
+  backendUser!: string;
+  backendPass!: string;
 
   public submitLogin(Username: string, Password: string){
+    this.userName = Username;
+    this.passWord = Password;
 
+    if(Username === this.userName && Password === this.passWord){
+      console.log('Login Passed to Backend')
+      if(this.userName && this.passWord === this.backendUser && this.backendPass){
+        console.log('Login Success!')
+      }else{
+        alert('Invalid Login Information. \nPlease Try Again or Reset Your Password')
+      }
+    }
+    else{
+      if(window.confirm('Login Information Validation Failed \nPlease Report this Message to Development Team.')){
+
+      }else{
+
+      }
+      
+    }
   }
-  constructor() { }
+  constructor(router: Router) { }
 
   ngOnInit(): void {
   }
