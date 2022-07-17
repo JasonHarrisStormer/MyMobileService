6f60cd294a2401a08df1597f23b60d7021a7cd84
Jason Harris <jharris@skillstorm.com>
2022-07-17 13:56:11 -0400

    updated plans and poms

--- frontend/src/app/utils/plans.ts
+++ frontend/src/app/utils/plans.ts
@@ -5,8 +5,9 @@ export const PLANS: Plan[] = [
   {
 
     type: "MyMobileService MORE POWER PLAN",
-    price: "$80/month per line",
+    price: "$80/month",
     properties: "Unlimited talk, text & high-speed data with no data caps!",
+    linelimit: "8 ($10 per month per additional line, $20 for each line over 8 per month)",
     access: "5G access included (where available)",
     hotspot: "Unlimited Hotspot data per line per month (throttled over 250GB per month)",
     text: "Unlimited texting from U.S. to 200+ countries",
@@ -15,8 +16,9 @@ export const PLANS: Plan[] = [
   },
   {
     type: "MyMobileService UNLIMITED SUPERIOR PLAN",
-    price: "$50/month per line",
+    price: "$50/month",
     properties: "Unlimited talk, text & high-speed data that can't slow down based on how much you use",
+    linelimit: "5 ($15 per month per additional line, $30 for each line over 5 per month)",
     access: "5G access included",
     hotspot: "50GB Hotspot data per line per month",
     text: "Unlimited texting from U.S. to 200+ countries",
@@ -24,8 +26,9 @@ export const PLANS: Plan[] = [
   },
   {
     type: "MyMobileService UNLIMITED SPECIAL PLAN",
-    price: "$40/month per line",
+    price: "$40/month",
     properties: "Unlimited talk, text, data + 50GB of Superior Data",
+    linelimit: "4 ($20 per month per additional line, $40 for each line over 4 per month)",
     access: "5G access included",
     hotspot: "15GB Hotspot data per line per month",
     text: "Unlimited texting from U.S. to 200+ countries",
@@ -33,8 +36,9 @@ export const PLANS: Plan[] = [
   }, 
   {
     type: "MyMobileService UNLIMITED PIONEER PLAN",
-    price: "$35/month per line",
+    price: "$35/month",
     properties: "Unlimited talk, text & data",
+    linelimit: "3 ($20 per month per additional line, $40 for each line over 3 per month)",
     access: "5G access included",
     hotspot: "3GB Hotspot data per line per month",
     text: "Unlimited texting from U.S. to 200+ countries",
