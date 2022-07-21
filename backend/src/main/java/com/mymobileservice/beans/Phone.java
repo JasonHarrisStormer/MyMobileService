b8fe97bef44e68d69a4db48701734451aff6f550
Jason Harris <jharris@skillstorm.com>
2022-07-21 13:23:36 -0400

    fixing

--- backend/src/main/java/com/mymobileservice/beans/Phone.java
+++
@@ -1,5 +0,0 @@
-package com.mymobileservice.beans;
-
-public class Phone {
-
-}
--- backend/src/main/java/com/mymobileservice/data/LineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mymobileservice.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mymobileservice.beans.Line;
-
-public interface LineRepository extends JpaRepository<Line, Integer> {
-
-}
