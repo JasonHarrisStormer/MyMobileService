56f6824560fc7da6612829c3127895574300c1c6
Jason Harris <jharris@skillstorm.com>
2022-07-26 14:40:54 -0400

    removed password from account

--- backend/src/main/java/com/mymobileservice/services/UsersService.java
+++ backend/src/main/java/com/mymobileservice/services/UsersService.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
