9da575db51b879e4b5f96d29f9b2b699ce771a6b
Jason Harris <jharris@skillstorm.com>
2022-07-26 14:00:45 -0400

    changed db to add balance table

--- backend/src/main/java/com/mymobileservice/services/UsersService.java
+++ backend/src/main/java/com/mymobileservice/services/UsersService.java
@@ -33,8 +33,8 @@ public class UsersService {
 	
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public Users add(Users users) {
-		Users dbAccount = repo.save(users);
-		return dbAccount;
+		Users dbUsers = repo.save(users);
+		return dbUsers;
 	}
 	
 	public Users findByEmailLike(String email){
