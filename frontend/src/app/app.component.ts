50926074a14229fb77dae7dfff5552d4e64a761f
dafeagins <dafeagins8715@gmail.com>
2022-07-28 13:37:53 -0500

    Log out button

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, UrlTree } from '@angular/router';
 
 @Component({
@@ -6,7 +6,7 @@ import { Router, UrlTree } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MyMobileService';
 
   //define drop down menu json info
@@ -40,11 +40,28 @@ export class AppComponent {
 
   clientHeight: number;
 
-
+  loginItem :any;
+  isLoggedIn : boolean = false;
+  ngOnInit(): void {
+    this.loginItem = localStorage.getItem("account");
+   
+   const id = JSON.parse(this.loginItem);
+   console.log(id);
+   if(id!=null)
+   this.isLoggedIn=true;
+    else
+    this.isLoggedIn=false;
+    console.log(this.isLoggedIn);
+ }
  constructor(private router: Router) {
     this.clientHeight = window.innerHeight; 
  }
  public dropClick(event: { itemData: { address: string | UrlTree; }; }) {
     this.router.navigateByUrl(event.itemData.address)  
+ }
+
+ public logoff(){
+  localStorage.clear();
+  console.log("clicked");
  }
 }
